9c2480e8cb63203b07444e27daf0f89e3e44ff12
kevinptx <kevin.peery@gmail.com>
2018-07-29 14:21:45 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,43 @@
 class Artist
+  attr_reader :name, :years_active
 
-  attr_reader :name
+  @@all = []
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  #get list of all of the paintings by specific artist
+  def list_paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  #Get a list of all the galleries that a specific artist has paintings in
+  def list_galleries
+    list_paintings.map do |gallery_painting|
+      gallery_painting.gallery
+    end
+  end
+
+  #Get a list of all cities that contain galleries that a specific artist has paintings in
+  def list_cities
+    list_galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  #Find the average years of experience of all artists
+  def self.years_experience
+    total_experience = self.all.map do
+      |artist| artist.years_active     end
+    total_experience.reduce(:+) / (self.all.count)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,21 @@
-require_relative '../config/environment.rb'
+require_relative "../config/environment.rb"
 
 def reload
-  load 'config/environment.rb'
+  load "config/environment.rb"
 end
 
+gallery1 = Gallery.new("Gallery1", "Houston")
+gallery2 = Gallery.new("Gallery2", "NY")
+gallery3 = Gallery.new("Gallery3", "LA")
+
+artist1 = Artist.new("Van Gogh", 20)
+artist2 = Artist.new("Rembrandt", 40)
+artist3 = Artist.new("Leonardo da Vinci
+", 20)
+
+# def initialize(title, style, artist, gallery)
+painting1 = Painting.new("Angst", "Impressionist", artist1, gallery1)
+painting2 = Painting.new("ScaryPainting", "ThisStyle", artist2, gallery2)
+
 binding.pry
+0
