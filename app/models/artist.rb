6ee131a5a7e8d9908a1a4cb8b5a21c8174472e63
github username <github email address>
2018-07-30 14:04:39 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,42 @@
 class Artist
-
-  attr_reader :name
-
+  attr_reader :name, :years_active
+  
+  ALL = []
+  
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
   end
 
-
+  def self.all 
+    ALL 
+  end
+  
+  def paintings
+    Painting.all.select do |painting_obj|
+      painting_obj.artist == self
+    end
+  end
+  
+  def galleries
+    self.paintings.map do |painting_obj|
+      painting_obj.gallery
+    end
+  end
+  
+  def cities
+    all_cities = self.galleries.map do |gallery_obj|
+      gallery_obj.city
+    end
+    
+    all_cities.uniq
+  end
+  
+  def self.experience
+    sum = self.all.reduce(0) do |total, artist_obj|
+      total += artist_obj.years_active
+    end
+    
+    sum / self.all.count
+  end
 end
