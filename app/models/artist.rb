db290f8b717584e631c4f60ceb9b4422087b9e3f
danrodz <daniel.a.rodriguez@dr.com>
2018-08-04 12:24:27 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,15 @@
 class Artist
-
   attr_reader :name
+  @@all = []
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,10 +1,14 @@
 class Painting
-
   attr_reader :title, :style
+  @@all = []
 
   def initialize(title, style)
     @title = title
     @style = style
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 end
